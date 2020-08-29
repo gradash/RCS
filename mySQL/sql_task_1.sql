174c06c46b1bf6694c1e50fcc5bcb811620f5713
gradash <gradash@gmail.com>
2020-08-29 15:30:13 +0300

    Update sql_task_1.sql

--- mySQL/sql_task_1.sql
+++ mySQL/sql_task_1.sql
@@ -1,9 +1,42 @@
-SELECT * FROM university.students;
+CREATE TABLE faculties (
 
-SELECT firstName, lastName FROM university.students;
+facultyID INT PRIMARY KEY AUTO_INCREMENT,
+facultyName VARCHAR(20) NOT NULL,
+facultyDesc VARCHAR(20) NOT NULL
 
-SELECT * FROM university.students WHERE course = 2;
 
-SELECT * FROM university.students WHERE course = 1 OR course = 3;
+);
 
-SELECT * FROM university.students WHERE firstName = 'Daniel';
+INSERT INTO faculties (facultyName, facultyDesc)
+VALUES
+('ExcavatorDriver', 'Who drives excavator'),
+('RocketLauncher', 'Who launch rockets');
+
+
+ALTER TABLE employees
+ADD COLUMN facultyID int;
+
+ALTER TABLE employees
+ADD CONSTRAINT fk_team_id
+FOREIGN KEY (facultyID) REFERENCES faculties(facultyID);
+
+UPDATE employees 
+SET facultyID = 1
+WHERE employeeId = 1;
+
+UPDATE employees 
+SET facultyID = 2
+WHERE employeeId = 2;
+
+UPDATE employees 
+SET facultyID = 2
+WHERE employeeId = 3;
+
+
+SELECT employees.firstName, employees.lastName, faculties.facultyName
+FROM employees, faculties
+WHERE faculties.facultyID = employees.facultyID AND employees.facultyID = 2;
+
+SELECT employees.firstName, employees.lastName, faculties.facultyName, faculties.facultyDesc
+FROM employees, faculties
+WHERE faculties.facultyID = employees.facultyID
