20a083f9e89f2ce4e2b93630d22c0746c5206ded
matvejevs <matvejevs@MATVEJEVS-W>
2020-09-08 11:13:40 +0300

    last

---
+++ PetShop/Connection.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace Day17_SampleSQL
+{
+    class Connection
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+
+        public Connection()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            server = "34.89.62.152"; // https://console.cloud.google.com/sql/instances/petshop/overview?project=crypto-abode-270012
+            database = "petshop";
+            uid = "root";
+            password = "pollux";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+
+        }
+
+
+
+
+
+
+        public bool Open()
+        {
+            try
+            {
+                connection.Open();
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool Close()
+        {
+            try
+            {
+                connection.Close();
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+
+
+
+}
+
+    
+
+/*
+        public void Select()
+        {
+            List<Jobs> jobLst = new List<Jobs>();
+
+            String query = "SELECT jobName, jobDescription FROM jobs";
+
+            if (this.Open() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    jobLst.Add(new Jobs(reader["jobName"].ToString(), reader["jobDescription"].ToString()));
+
+                }
+
+                reader.Close();
+
+                this.Close();
+
+                foreach (Jobs job in jobLst)
+                {
+                    Console.WriteLine(job.jobName + "," + job.jobDescr);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nesanak savienoties!");
+            }
+        }
+
+        public void Insert()
+        {
+            String query = "INSERT INTO jobs (jobName, jobDescription)" +
+                " VALUES('Elektronikis', 'Elektribas burvis')";
+            if (this.Open() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+
+                this.Close();
+            }
+            else
+            {
+                Console.WriteLine("Nesanak savienoties!");
+            }
+        }
+
+      
+    }
+
+}
+*/
