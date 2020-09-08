577c31279e3b79c7e406e37656ac6623d1c6e066
matvejevs <matvejevs@MATVEJEVS-W>
2020-09-08 10:49:23 +0300

    petshop1

---
+++ PetShop/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace PetShop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.showPets = new System.Windows.Forms.Button();
+            this.addPet = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // showPets
+            // 
+            this.showPets.Location = new System.Drawing.Point(86, 73);
+            this.showPets.Name = "showPets";
+            this.showPets.Size = new System.Drawing.Size(107, 23);
+            this.showPets.TabIndex = 0;
+            this.showPets.Text = "Show Pets";
+            this.showPets.UseVisualStyleBackColor = true;
+            this.showPets.Click += new System.EventHandler(this.showPets_Click);
+            // 
+            // addPet
+            // 
+            this.addPet.Location = new System.Drawing.Point(199, 73);
+            this.addPet.Name = "addPet";
+            this.addPet.Size = new System.Drawing.Size(107, 23);
+            this.addPet.TabIndex = 1;
+            this.addPet.Text = "button2";
+            this.addPet.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(312, 73);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(234, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Pet Shop";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(45, 111);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(409, 182);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 305);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.addPet);
+            this.Controls.Add(this.showPets);
+            this.Name = "Form1";
+            this.Text = "Pet Shop";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button showPets;
+        private System.Windows.Forms.Button addPet;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ PetShop/Form2.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace PetShop
+{
+    partial class petlistForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.petList = new System.Windows.Forms.ListBox();
+            this.addPet = new System.Windows.Forms.Button();
+            this.sellPet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // petList
+            // 
+            this.petList.FormattingEnabled = true;
+            this.petList.Location = new System.Drawing.Point(12, 12);
+            this.petList.Name = "petList";
+            this.petList.Size = new System.Drawing.Size(319, 316);
+            this.petList.TabIndex = 0;
+            // 
+            // addPet
+            // 
+            this.addPet.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.addPet.Location = new System.Drawing.Point(360, 87);
+            this.addPet.Name = "addPet";
+            this.addPet.Size = new System.Drawing.Size(113, 23);
+            this.addPet.TabIndex = 1;
+            this.addPet.Text = "Add Pet";
+            this.addPet.UseVisualStyleBackColor = true;
+            // 
+            // sellPet
+            // 
+            this.sellPet.Location = new System.Drawing.Point(360, 130);
+            this.sellPet.Name = "sellPet";
+            this.sellPet.Size = new System.Drawing.Size(113, 23);
+            this.sellPet.TabIndex = 2;
+            this.sellPet.Text = "Sell Pet";
+            this.sellPet.UseVisualStyleBackColor = true;
+            // 
+            // petlistForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 338);
+            this.Controls.Add(this.sellPet);
+            this.Controls.Add(this.addPet);
+            this.Controls.Add(this.petList);
+            this.Name = "petlistForm";
+            this.Text = "Pet List";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox petList;
+        private System.Windows.Forms.Button addPet;
+        private System.Windows.Forms.Button sellPet;
+    }
+}
