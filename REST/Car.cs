b70100d4fd254d8711af464a3b8b3686b6d988f9
gradash <gradash@gmail.com>
2020-09-14 18:15:57 +0300

    123

---
+++ REST/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace REST
+{
+    class Car
+    {
+
+        public string mark;
+        public string model;
+        public string color;
+
+
+        public Car(string mark, string model, string color)
+        {
+
+            this.mark = mark;
+            this.model = model;
+            this.color = color;
+
+        }
+
+
+
+
+        public void printInfo()
+        {
+            Console.WriteLine(mark);
+            Console.WriteLine(model);
+            Console.WriteLine(color);
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ REST/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace REST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String url = "https://my-json-server.typicode.com/gradash/RCS/cars";
+
+            WebClient client = new WebClient();
+
+            String response = client.DownloadString(url);
+
+            
+
+
+            List<Car> rec = JsonConvert.DeserializeObject<List<Car>>(response);
+
+           
+            
+                foreach (Car c in rec)
+                {
+                    Console.WriteLine(c.mark +" "+ c.model +" "+ c.color);
+                }
+            
+
+
+
+        }
+    }
+}
